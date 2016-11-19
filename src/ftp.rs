7200a3f5ecd14a458c2ef5ba9c4a86ed17d15469
Zach <zach_41@163.com>
2016-11-19 20:30:37 +0800

    login, cwd, pwd, noop, mkdir, pasv, rmdir, rm, quit, rename, size

--- src/ftp.rs
+++ src/ftp.rs
@@ -1,6 +1,7 @@
 use std::io as stdio;
 use std::io::{Read, Write, BufReader, BufWriter, BufRead, Stderr};
 use std::net::{TcpStream, SocketAddr, ToSocketAddrs};
+use std::str::FromStr;
 use regex::Regex;
 use super::status;
 use super::types::{FileType, FtpError, Line, Result};
@@ -77,5 +78,147 @@ impl FtpStream {
             })
     }
 
-    
+    /// Log into the FTP server
+    pub fn login(&mut self, username: &str, passwd: &str) -> Result<()> {
+        let username_cmd = format!("USER {}\r\n", username);
+        try!(self.write_str(&username_cmd));
+
+        self.read_response_in(&[status::LOGGED_IN, status::NEED_PASSWORD])
+            .and_then(|Line(code, _)| {
+                if code == status::NEED_PASSWORD {
+                    let passwd_cmd = format!("PASS {}\r\n", passwd);
+                    try!(self.write_str(&passwd_cmd));
+                    try!(self.read_response(status::LOGGED_IN));
+                }
+                Ok(())
+            })
+    }
+
+    /// Change the current working directory
+    pub fn cwd(&mut self, path: &str) -> Result<()> {
+        let cwd_cmd = format!("CWD {}\r\n", path);
+        try!(self.write_str(&cwd_cmd));
+        self.read_response(status::REQUESTED_FILE_ACTION_OK).map(|_| ())
+    }
+
+    /// Move to the parent directory
+    pub fn cdup(&mut self) -> Result<()> {
+        let cdup_cmd = format!("CDUP\r\n");
+        try!(self.write_str(&cdup_cmd));
+        self.read_response(status::REQUESTED_FILE_ACTION_OK).map(|_| ())
+    }
+
+    /// Get the current working directory
+    pub fn pwd(&mut self) -> Result<String> {
+        let pwd_cmd = format!("PWD\r\n");
+        try!(self.write_str(&pwd_cmd));
+
+        self.read_response(status::PATH_CREATED)
+            .and_then(|Line(_, line)| {
+                match (line.find('"'), line.rfind('"')) {
+                    (Some(begin), Some(end)) if begin < end => {
+                        Ok(line[begin+1 .. end].to_string())
+                    }
+                    _ => {
+                        let cause = format!("Invalid PWD Response: {}", line);
+                        Err(FtpError::InvalidResponse(cause))
+                    }
+                }
+            })
+    }
+
+    /// NOOP command
+    pub fn noop(&mut self) -> Result<()> {
+        let noop_cmd = format!("NOOP\r\n");
+        try!(self.write_str(&noop_cmd));
+
+        self.read_response(status::COMMAND_OK).map(|_| ())
+    }
+
+    /// Creates a new directory
+    pub fn mkdri(&mut self, path: &str) -> Result<()> {
+        let mkdir_cmd = format!("MKD {}\r\n", path);
+        try!(self.write_str(&mkdir_cmd));
+
+        self.read_response(status::PATH_CREATED).map(|_| ())
+    }
+
+    /// Switches to Passive mode
+    pub fn pasv(&mut self) -> Result<SocketAddr> {
+        try!(self.write_str("PASV\r\n"));
+        let Line(_, line) = try!(self.read_response(status::PASSIVE_MODE));
+        PORT_RE.captures(&line)
+            .ok_or(FtpError::InvalidResponse(format!("Invalid PASV Response: {}", line)))
+            .and_then(|caps| {
+                let (oct1, oct2, oct3, oct4) = (
+                    caps[1].parse::<u8>().unwrap(),
+                    caps[2].parse::<u8>().unwrap(),
+                    caps[3].parse::<u8>().unwrap(),
+                    caps[4].parse::<u8>().unwrap(),
+                );
+                let (msb, lsb) = (
+                    caps[5].parse::<u8>().unwrap(),
+                    caps[6].parse::<u8>().unwrap(),
+                );
+                let port = ((msb as u16) << 8) + lsb as u16;
+                let addr = format!("{}.{}.{}.{}:{}", oct1, oct2, oct3, oct4, port);
+
+                if cfg!(feature = "debug_print") {
+                    try!(stdio::stderr().write_fmt(format_args!("PASV Addr: {}\n", addr)));
+                }
+
+                SocketAddr::from_str(&addr).map_err(|parse_err| {
+                    FtpError::InvalidAddress(parse_err)
+                })
+            })
+    }
+
+    /// Quits the current FTP session
+    pub fn quit(&mut self) -> Result<()> {
+        let quit_cmd = format!("QUIT\r\n");
+        try!(self.write_str(&quit_cmd));
+
+        self.read_response(status::CLOSING).map(|_| ())
+    }
+
+    /// Renames the file from from_name to to_name
+    pub fn rename(&mut self, from_name: &str, to_name: &str) -> Result<()> {
+        let rnfr_cmd = format!("RNFR {}\r\n", from_name);
+        try!(self.write_str(&rnfr_cmd));
+        self.read_response(status::REQUEST_FILE_PENDING)
+            .and_then(|_| {
+                let rnto_cmd = format!("RNTO {}\r\n", to_name);
+                try!(self.write_str(&rnto_cmd));
+                self.read_response(status::REQUESTED_FILE_ACTION_OK).map(|_| ())
+            })
+    }
+
+    /// Removes a directory
+    pub fn rmdir(&mut self, path: &str) -> Result<()> {
+        let rmdir_cmd = format!("RMD {}\r\n", path);
+        try!(self.write_str(&rmdir_cmd));
+
+        self.read_response(status::REQUESTED_FILE_ACTION_OK).map(|_| ())
+    }
+
+    /// Removes a file
+    pub fn rm(&mut self, filename: &str) -> Result<()> {
+        let rm_cmd = format!("DELE {}\r\n", filename);
+        try!(self.write_str(&rm_cmd));
+
+        self.read_response(status::REQUESTED_FILE_ACTION_OK).map(|_| ())
+    }
+
+    /// Gets the size of file in bytes, if file doesn't exists, return None
+    pub fn size(&mut self, pathname: &str) -> Result<Option<usize>> {
+        let size_cmd = format!("SIZE {}\r\n", pathname);
+        try!(self.write_str(&size_cmd));
+
+        let Line(_, line) = try!(self.read_response_in(&[status::FILE, status::FILE_UNAVAILABLE]));
+
+        match SIZE_RE.captures(&line) {
+            Some(caps) => Ok(Some(caps[1].parse::<usize>().unwrap())),
+            None => Ok(None),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
 extern crate FTPCLI;
 
 use FTPCLI::FtpStream;
+use FTPCLI::FtpError;
 
 fn main() {
-    let mut ftpStream = FtpStream::connect("127.0.0.1:21");
+    let mut ftpStream = FtpStream::connect("182.254.245.238:21").unwrap();
+
+    ftpStream.login("zach", "admin123123").unwrap();
+
+    // ftpStream.cwd("~/hello").unwrap();
+
+    // ftpStream.cdup().unwrap();
+
+    ftpStream.pwd().map(|pwd| println!("{}", pwd));
+
+    ftpStream.noop().unwrap();
+
+    ftpStream.mkdri("hello2");
+    
+    ftpStream.pasv().unwrap();
+
+    ftpStream.rename("hello2", "hello");
+
+    // ftpStream.rmdir("hello").unwrap();
+
+    ftpStream.size("demo.c").unwrap();
+    ftpStream.size("hello.txt").map(|ret| {
+        match ret {
+            Some(size) => println!("FILE SIZE: {}", size),
+            None => println!("FILE NOT EXISTS"),
+        }
+    });
+
+    // ftpStream.rm("test.txt").unwrap();
+    ftpStream.quit().unwrap();
 }
